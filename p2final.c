544cd1b28e381f272e011df0eb787e0d4f6d78c7
Kushal <95837058+Kush-007@users.noreply.github.com>
2021-12-31 03:52:24 +0000

    gcc p2debuglog.txt

---
+++ p2final.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int input()
+{
+  int a;
+  printf("enter value");
+  scanf("%d",&a);
+  return a;
+}
+
+int add(int a,int b)
+{
+  int c;
+  c=a+b;
+  return c;
+}
+
+int output(int a,int b,int c)
+{
+  printf("sum of two nunbers%d+%d=%d",a,b,c);
+}
+
+int main()
+{
+  int a,b,sum;
+  a=input();
+  b=input();
+  sum=add(a,b);
+  output(a,b,sum);
+  return 0;
+}
